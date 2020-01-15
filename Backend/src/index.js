c2f31b7c94f58a19604281b248cfbd6ed5844793
Fabinhooh <58363774+Fabinhooh@users.noreply.github.com>
2020-01-15 14:45:49 -0300

    OmniStack dia 2 terminado sem exercício.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,10 +4,12 @@ const routes = require('./routes')
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-k7ei5.mongodb.net/week10?retryWrites=true&w=majoritymongodb+srv://omnistack:omnistack@cluster0-k7ei5.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-k7ei5.mongodb.net/week10', {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
-})
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
 
 app.use(express.json());
 app.use(routes);
